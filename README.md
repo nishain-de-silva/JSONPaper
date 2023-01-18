0f572bc8109161696a12d0546a62173e133d2118
nishain <nishain.atomic@gmail..com>
2023-01-18 11:29:18 +0530

    added entries functions, comments and additional utility function

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-# JSONStore
+# JSONStore (v 1.0)
 
-Swift is a type constrained lanuage right ? The default way of parsing JSON is to parse json string known `Codable` JSON structure. But most of the time you may put hand on unknown JSON structures still need to access values. Store help you parse JSON in any free form and extract value without constraining into a fixed type. This is pure zero dependency package with simplistic `Read Only Until Value Dicovered` design in mind in a single character-by-character read cycle and theorically should be performant.
+Swift is a type constrained lanuage right ? The default way of parsing JSON is to parse json string to a known `Codable` JSON structure. But most of the time you may put hand on unknown JSON structures and difficult to define those structures for all scenarios. JSONStore help you to parse JSON in any free form and extract value without constraining into a fixed type. This is pure zero dependency `Swift` package with simplistic `Read Only Until Value Dicovered` design in mind in a single read cycle and theorically should be performant.
+
+## Installation
+
+You can use `Swift Package Manager` to Install `JSONStore` from this repository url. Thats it!
 
 ## Usage
 
-> **`warning`** - you need to be 100% sure the input JSON string input is in correct `JSON`  format otherwise would give you incorrect or unexpected value.
+> **`warning`** - You need to be 100% sure the input JSON string input is in correct `JSON` format otherwise would give you incorrect or unexpected value.
 
-to access attribute or element you can you a simple `string` path seperated by **`.`** notation.
+To access attribute or element you can you a simple `string` path seperated by **`.`** notation.
 ```swift
 import JSONStore
+
+let jsonText = String(data: apiDataBytes, encoding: .utf8)
 
 let nameValue:String = JSONEntity(jsonText).text("properyA.properyB.2") ?? "default value"
 ```
@@ -19,19 +25,23 @@ and then use methods. You can use the following methods to access value of given
 - `bool()`
 - `number()`
 - `isNull()`
+- `isExist()`
+- `entries()`
 
-If the key does not exist in JSON or in intermediate value or if the value is another data type from expected formated then `nill` would be given. You dont't need to worry about optional chaining
+If the key / index does not exist in given path or if the value is another data type from expected formated (except for `text()` see [Exceptions](#exceptional-situations)) then `nill` would be given. You don't need to worry about optional chaining you will recieve `nil` if intermediate path also did not exist.
 
 example:
 ```swift
-JSONEntity(jsonText).number("peopple.2.details.age") // return age
+JSONEntity(jsonText).number("people.2.details.age") // return age
 
-JSONEntity(jsonText).number("peopple.2.details.name") // return nil since name is not a number
+JSONEntity(jsonText).number("people.2.wrongKey.details.age") // return nil
+
+JSONEntity(jsonText).number("people.2.details.name") // return nil since name is not a number
 ```
 
 To check if an value is `null` use `isNull()` method don't assume `nil` as `null` it could be value does not exist at all. `isNull` gives `boolean` based on value is actually `null` or or `nil` if value is not found.
 
-### Iterating values
+### Iterating values in array
 
 If you need to acesss value is an array you can use:
 ```swift
@@ -39,14 +49,34 @@ guard let hobbies:[JSONEntity] = JSONEntity(jsonText).array("people.2.hobbies") 
 
 hobbies[0].string() // hobby 1
 ```
+### Iterating values in object
+You can also iterate values and keys in JSON objects as `tuple` of `key-value` pair. Suppose we have JSON like this
 
-You can also keep record intermediate value `instance` for later use.
+```json
+{
+    "person": {
+        "name": "Sam",
+        "age": 25,
+        "additionalData": null
+    }
+}
+```
+
+
+```swift
+guard let entries = JSONEntity(jsonText).entries("person") else { return "NotAnObject" }
+
+let attributeKey = entries[0].key // name
+let values = entries[0].value.text() // Sam
+```
+
+You can also keep record of intermediate `reference instance` for later use and do chaining.
 
 ```swift
 
-let pointer = JSONEntity(jsonText).object("pathA.pathB")
+let pointer = JSONEntity(jsonText).object("pathA.pathB") // return JSONEntity
 
-let keyValue: String? = pointerA.text("pathC.key1")
+let keyValue: String? = pointer.text("pathC.key1")
 
 ```
 > Note that this would not change reference of `pointer` as `.object()` deliver new `JSONEntity` object.
@@ -60,7 +90,7 @@ let keyValue: String? = pointerA.text("pathC.key1")
     In JSON data if keyB happens to be an `string` or another primitive type instead of container type of either `object` or `array` then last found intermediete primitive value (in this case string value of keyB) will be return instead of `nil`.
 
 - The result of `.object()` doesn't need necessarily need to be an JSON object but it can be `array` either and you can still access with `.object(somePath).text('2.name')`
-- if you are not sure of sure of data type particular of an attribute or `debugging` pourpose you can always use `.text(attributePath)` as it always give the value as a `string` format unless attribute was not found which would give `nil`.
+- if you are not sure of sure of data type particular of an attribute or `dumping` pourpose you can always use `.text(attributePath)` as it always give the value as a `string` format unless attribute was not found which would give `nil`.
 
 ### Author and Main Contributor
 @Nishain De Silva
--- Sources/JSONStore/JSONStore.swift
+++ Sources/JSONStore/JSONStore.swift
@@ -1,6 +1,7 @@
 public class JsonEntity {
     private var jsonText: String
     private var arrayValues:[String]? = nil
+    private var objectEntries:[(key:String, value:String)]? = nil
     
     public init(_ json:String) {
         jsonText = json
@@ -41,16 +42,34 @@ public class JsonEntity {
         }
     }
     
-    public func array(_ path:String) -> [JsonEntity]? {
+    public func array(_ path:String? = nil) -> [JsonEntity]? {
         arrayValues = []
-        if decodeData("\(path).-1") != "$COMPLETE_ARRAY" { return nil }
-        return arrayValues!.map({value in
+        if decodeData(path == nil ? "-1" : "\(path!).-1") != "$COMPLETE_ARRAY" {
+            arrayValues = nil
+            return nil
+        }
+        let results = arrayValues!.map({value in
             return JsonEntity(value)
         }) as [JsonEntity]
+        arrayValues = nil
+        return results
     }
     
-    public func asJsonText() -> String {
-        return jsonText
+    public func isExist(_ path:String) -> Bool {
+        return decodeData(path) != nil
+    }
+    
+    public func entries(_ path:String) -> [(key: String, value: JsonEntity)]? {
+        objectEntries = []
+        if decodeData("\(path).dummyAtr") != "$COMPLETE_OBJECT" {
+            objectEntries = nil
+            return nil
+        }
+        let results = objectEntries!.map({ value in
+            return (value.key, JsonEntity(value.value))
+        }) as [(String, JsonEntity)]
+        objectEntries = nil
+        return results
     }
     
     private func decodeData(_ inputPath:String) -> String? {
@@ -67,20 +86,21 @@ public class JsonEntity {
         var isCountArray = false
         var isGrabbingMultipleValues = false
         
-        var elementIndexCursor = -1
-        var pathArrayIndex = -1
+        var elementIndexCursor = -1 // the count variable when iterating array
+        var pathArrayIndex = -1 // the array index of given on path
         var notationBalance = 0
         
         let copyArrayData:Bool = arrayValues != nil
+        let copyObjectEntries:Bool = objectEntries != nil
         
         for char in jsonText {
             // if within quotation ignore processing json literals...
-                        
             if !isInQuotes {
                 if char == "{" || char == "[" {
                     notationBalance += 1
                     
                     if isCountArray {
+                        // ignore processing if element in not matching the array index
                         if elementIndexCursor != pathArrayIndex {
                             if isGrabbingMultipleValues { grabbedText.append(char) }
                             continue
@@ -88,17 +108,21 @@ public class JsonEntity {
                         processedPathIndex += 1
                         isCountArray = false
                     }
+                    // if the last value of last key is object or array then start copy it
                     if processedPathIndex == paths.count && !isGrabbingNotation {
                         grabbedText = ""
                         isGrabbingNotation = true
                     }
+                    // continue copying object/arrray notation...
                     if isGrabbingNotation {
                         grabbedText.append(char)
                         continue
                     }
                     
+                    // starting to count elements in array on reaching open bracket...
                     if char == "[" && !isCountArray && (processedPathIndex + 1) == notationBalance {
                         let parsedIndex = Int(paths[processedPathIndex])
+                        // occur when trying to access element of array with non-number index
                         if parsedIndex == nil {
                             return nil
                         }
@@ -106,10 +130,12 @@ public class JsonEntity {
                         pathArrayIndex = parsedIndex!
                         elementIndexCursor = 0
                         startSearchValue = true
+                        // start to copy all element in array on last -1 index
                         if copyArrayData && (processedPathIndex + 1) == paths.count {
                             isGrabbingMultipleValues = true
                         }
                     } else {
+                        // upon meeting open 'notation' searching for next key on next path should start
                         startSearchValue = false
                     }
                     
@@ -119,20 +145,40 @@ public class JsonEntity {
                 if char == "}" || char == "]" {
                     notationBalance -= 1
                     
+                    // if a primitive value is in proccess copying then return copied value
                     if isGrabbingText {
-                        return grabbedText                    }
+                        // when finished copy last primitive value on copyObjectEntries mode. Need to make sure the parent container notation is an object
+                        if copyObjectEntries && char == "}" {
+                            objectEntries!.append((grabbingKey, grabbedText))
+                            return "$COMPLETE_OBJECT"
+                        }
+                        return grabbedText
+                    }
                     if isGrabbingNotation { grabbedText.append(char) }
+                    
+                    // occur after all element in foccused array or object is finished searching...
                     if notationBalance == processedPathIndex {
                         if isCountArray {
+                            // occur when when not matching element is found for given array index and array finished iterating...
                             if isGrabbingMultipleValues {
                                 arrayValues!.append(grabbedText)
                                 return "$COMPLETE_ARRAY"
                             }
                             return nil
                         }
-                        if char == "}" && !startSearchValue { return nil }
+                        // exit occur after no matching key is found in object
+                        if char == "}" && !startSearchValue {
+                            if copyObjectEntries { return "$COMPLETE_OBJECT" }
+                            return nil
+                        }
+                        
+                        // occur after finishing copy json notation
                         if processedPathIndex == paths.count {
-                            return grabbedText
+                            if !copyObjectEntries { return grabbedText }
+                            objectEntries!.append((grabbingKey, grabbedText))
+                            startSearchValue = false
+                            isGrabbingNotation = false
+                            processedPathIndex -= 1
                         }
                     }
                     if isGrabbingMultipleValues { grabbedText.append(char) }
@@ -144,7 +190,9 @@ public class JsonEntity {
                 }
             }
             
+            // isGrabbingMultipleValues is flag to capture all elements in array
             if isGrabbingMultipleValues {
+                // after each element in array level there would be ',' character
                 if char == "," && (processedPathIndex + 1) == notationBalance {
                     arrayValues!.append(grabbedText)
                     grabbedText = ""
@@ -173,22 +221,32 @@ public class JsonEntity {
                         isInQuotes = !isInQuotes
                         isGrabbingText = !isGrabbingText
                         if !isGrabbingText {
-                            return grabbedText
+                            if !copyObjectEntries { return grabbedText }
+                            objectEntries!.append((grabbingKey, grabbedText))
+                            startSearchValue = false
+                            processedPathIndex -= 1
                         } else {
                             grabbedText = ""
                         }
+                        // used to copy values true, false, null and number
                     } else if !isInQuotes && !isGrabbingText && (char.isNumber || char == "n" || char == "f" || char == "t") {
+                        grabbedText = ""
                         grabbedText.append(char)
                         isGrabbingText = true
                         continue
                     } else if isGrabbingText {
                         if !isInQuotes && char == "," {
-                            return grabbedText
+                            if !copyObjectEntries { return grabbedText }
+                            objectEntries!.append((grabbingKey, grabbedText))
+                            startSearchValue = false
+                            isGrabbingText = false
+                            processedPathIndex -= 1
                         }
                         grabbedText.append(char)
                         continue
                     }
                 }
+                // section responsible for finding matching key in object notation
             } else {
                 // grabbing the matching correct object key as given in path
                 if (processedPathIndex + 1) == notationBalance && char == "\"" {
@@ -196,7 +254,7 @@ public class JsonEntity {
                     isInQuotes = !isInQuotes
                     if !isGrabbingKey {
                         // if found start searching for object value for object key
-                        if grabbingKey == paths[processedPathIndex] {
+                        if (copyObjectEntries && (processedPathIndex + 1) == paths.count) || grabbingKey == paths[processedPathIndex] {
                             processedPathIndex += 1
                             startSearchValue = true
                         }
